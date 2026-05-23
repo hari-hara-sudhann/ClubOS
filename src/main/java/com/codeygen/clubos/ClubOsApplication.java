ca9b4747f78993ada5ed5485f18f5a388b989950
hari-hara-1 <hari.shenbagan@gmail.com>
2026-05-23 13:33:58 +0530

    Migrated DB from h2 to postgres

--- src/main/java/com/codeygen/clubos/ClubOsApplication.java
+++ src/main/java/com/codeygen/clubos/ClubOsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClubOsApplication {
 
 	public static void main(String[] args) {
+		System.out.println(System.getenv("DATABASE_USERNAME"));
 		SpringApplication.run(ClubOsApplication.class, args);
 	}
 
